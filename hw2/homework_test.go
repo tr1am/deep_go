aa8e1cdac575ab00aa0d09c19b1573511000acd6
Vladimir Pravdin <pravdin31@gmail.com>
2025-07-15 15:23:38 +0200

    Homework #2: Circular Queue

--- hw2/homework_test.go
+++ hw2/homework_test.go
@@ -4,35 +4,33 @@ import (
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
-	"unsafe"
 )
 
 // go test -v homework_test.go
 
 type CircularQueue struct {
 	values []int
-	startq *int
-	endq   *int
+	startq int
+	endq   int
 	// need to implement
 }
 
 func NewCircularQueue(size int) CircularQueue {
 	var CQ CircularQueue
 	CQ.values = make([]int, size)
-	CQ.startq = &CQ.values[0]
-	CQ.endq = &CQ.values[0]
+	CQ.startq = 0
+	CQ.endq = 0
 	// init all elements
 	for i := 0; i < size; i++ {
 		CQ.values[i] = -1
 	}
 	return CQ
 }
-
 func (q *CircularQueue) Push(value int) bool {
 	// calculate pointer for next element and the pointer to the last element of slice
 	// to check borders of slice
-	next := (*int)(unsafe.Add(unsafe.Pointer(q.endq), (int)(unsafe.Sizeof(q.values[0]))))
-	last := &q.values[len(q.values)-1]
+	next := q.endq + 1
+	last := len(q.values) - 1
 
 	// Cannot add new element due to slice is full
 	if q.Full() {
@@ -41,53 +39,51 @@ func (q *CircularQueue) Push(value int) bool {
 	// Write the first value. Don't need to calculate new pointer
 	// It's already point to first empty element
 	if q.Empty() {
-		*q.endq = value
+		q.endq = 0
+		q.startq = 0
+		q.values[0] = value
 		return true
 	}
 	// move the End Queue pointer to the beginning and write first element
 	if (q.endq == last) && (q.values[0] == -1) {
-		q.endq = &q.values[0]
-		*q.endq = value
+		q.endq = 0
+		q.values[0] = value
 		return true
 	}
 	// move the End Queue pointer to next element and write it
-	if *next == -1 {
-		*next = value
+	if q.values[next] == -1 {
+		q.values[next] = value
 		q.endq = next
 		return true
 	}
 	return false
 }
-
 func (q *CircularQueue) Pop() bool {
 	if q.Empty() {
 		return false
 	}
 	// if it's the last element in the slice, move to the first element
-	if q.startq == &q.values[len(q.values)-1] {
-		*q.startq = -1
-		q.startq = &q.values[0]
+	if q.startq == len(q.values)-1 {
+		q.values[q.startq] = -1
+		q.startq = 0
 		return true
 	}
-	*q.startq = -1
-	q.startq = (*int)(unsafe.Add(unsafe.Pointer(q.startq), (int)(unsafe.Sizeof(q.values[0]))))
+	q.values[q.startq] = -1
+	q.startq++
 	return true
 }
-
 func (q *CircularQueue) Front() int {
 	if q.Empty() {
 		return -1
 	}
-	return *q.startq
+	return q.values[q.startq]
 }
-
 func (q *CircularQueue) Back() int {
 	if q.Empty() {
 		return -1
 	}
-	return *q.endq
+	return q.values[q.endq]
 }
-
 func (q *CircularQueue) Empty() bool {
 	for i := 0; i < len(q.values); i++ {
 		if q.values[i] != -1 {
@@ -96,7 +92,6 @@ func (q *CircularQueue) Empty() bool {
 	}
 	return true
 }
-
 func (q *CircularQueue) Full() bool {
 	for i := 0; i < len(q.values); i++ {
 		if q.values[i] == -1 {
